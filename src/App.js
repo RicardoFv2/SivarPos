9e530f9c9eb6053262bac6b962568e68d817dfd6
Ricardo <ricardisimofuentes@hotmail.com>
2023-08-21 14:55:28 -0600

    Sistema POS en React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
 import "./App.css";
-import Header from "./components/header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+import Menu from "./components/menu";
+import Index from "./components/routes/index.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/",
+  },
+]);
 
 function App() {
+  const [Metamask, setMetamask] = useState(false);
+
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  const conectarWallet = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const web3Instance = new Web3(window.ethereum);
+      setWeb3(web3Instance);
+
+      try {
+        await window.ethereum.enable();
+
+        const accounts = await web3Instance.eth.getAccounts();
+        const account = accounts[0]; //setea
+        console.log(accounts[0]);
+
+        setAccount(accounts[0]);
+
+        const balanceWei = await web3Instance.eth.getBalance(accounts[0]);
+        const balanceEth = web3Instance.utils.fromWei(balanceWei, "ether");
+        console.log(balanceEth);
+
+        setBalance(balanceEth);
+
+        
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      setMetamask(false);
+    }
+  };
+
+  useEffect(() => {
+    async function Wallet() {
+      if (typeof window.ethereum !== "undefined") {
+        console.log("Wallet: SI.");
+        setMetamask(true);
+      } else {
+        console.log("Wallet: NO");
+      }
+    }
+    Wallet();
+  }, []);
+
   return (
-    <div className="App">
-      <Header ></Header>
+    <div>
+      {Metamask ? (
+        <>
+          <RouterProvider router={router} />
+          conectarWallet={conectarWallet}
+          direccion={account}
+          saldo={balance}
+        </>
+      ) : (
+        <div>Instala metamask</div>
+      )}
     </div>
   );
 }
---
+++ src/components/menu.jsx
@@ -0,0 +1,40 @@
+import icoeth from './img/etereo.png';
+import icomm from './img/metamask.png';
+import { Link } from 'react-router-dom';
+
+
+const Menu = (props) => {
+    return (
+        <div>
+            <div className="navbar">
+                <div className='contenedor'>
+
+                    <div className="logo">Blockchain</div>
+                    <ul className="menu">
+                        <button onClick={props.conectarWallet} className="boton" value={props.account}>Conectar wallet</button>
+                    </ul>
+                </div>
+            </div>
+            <div className="navbarr">
+
+                <div className='contenedor'>
+                    <ul className="menu">
+                        <li><a id="contenido4" href="#">Inicio</a></li>
+
+
+                    </ul>
+
+                    <table>
+                        <tbody>
+                            <tr><div className='flex-row'><img className='icon' src={icomm}></img> {props.direccion}</div></tr>
+                            <tr><div className='flex-row'><img src={icoeth}></img> {props.saldo} ETH</div></tr>
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+
+        </div>
+    )
+};
+export default Menu; 
